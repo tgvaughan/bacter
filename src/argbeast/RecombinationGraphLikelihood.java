089f97527969564fdfc9adc6d06a03427ee25e60
Tim Vaughan <tgvaughan@gmail.com>
2013-07-19 10:48:12 +1200

    Added ARG likelihood class.

---
+++ src/argbeast/RecombinationGraphLikelihood.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package argbeast;
+
+import beast.core.Description;
+import beast.evolution.likelihood.TreeLikelihood;
+
+/**
+ *
+ * @author Tim Vaughan <tgvaughan@gmail.com>
+ */
+@Description("Probability of sequence data given recombination graph.")
+public class RecombinationGraphLikelihood extends TreeLikelihood {
+    
+}
