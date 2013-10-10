df4170e86770a51ed52db5ad0908ffd48d8f5fbd
Tim Vaughan <tgvaughan@gmail.com>
2013-10-10 19:49:59 +1300

    Working on coalescent model implementation.

---
+++ src/argbeast/GCCoalescentApprox.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2013 Tim Vaughan <tgvaughan@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package argbeast;
+
+import beast.core.Description;
+import beast.evolution.tree.coalescent.Coalescent;
+
+/**
+ * @author Tim Vaughan <tgvaughan@gmail.com>
+ */
+@Description("Appoximation to the coalescent with gene conversion.")
+public class GCCoalescentApprox extends Coalescent {
+    
+    RecombinationGraph arg;
+    
+    @Override
+    public void initAndValidate() throws Exception {
+        if (!(treeInput.get() instanceof RecombinationGraph))
+            throw new Exception("Error: GCCoalescentApprox only applicable to"
+                    + " RecombinationGraph objects.");
+        
+        arg = (RecombinationGraph)treeInput.get();
+        
+        super.initAndValidate();
+    }
+    
+    @Override
+    public double calculateLogP() throws Exception {
+        
+        return logP;        
+    }
+    
+    double clonalFrameLogP() {
+        return calculateLogLikelihood(treeIntervalsInput.get(),
+                popSizeInput.get());
+    }
+    
+    double conditionalCoalescentLogP() {
+        return 0.0;
+    }
+    
+    double convertedRegionLogP() {
+        return 0.0;
+    }
+    
+}
