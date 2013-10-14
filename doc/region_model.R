5cdae389be1a309948d926fc32bfea03a8640fb0
Tim Vaughan <tgvaughan@gmail.com>
2013-10-14 13:57:02 +1300

    Added figure to model description.

--- doc/region_model.R
+++ doc/region_model.R
@@ -1,3 +1,4 @@
+# Simulate generation of regions along sequence of length L
 sim <- function (rho, delta, L) {
 
     svec <- rep(0,L)
@@ -34,3 +35,30 @@ sim <- function (rho, delta, L) {
 
     return(res)
 }
+
+
+# Plot dependence of expected number of regions against rho and delta
+# (rho is in units of tree length)
+
+expectedCount <- function(rho, delta, L) {
+    return ((rho/2)/(rho*delta/(2*L) + 1))
+}
+
+
+pdf('regionCount.pdf', width=7, height=5)
+
+rho <- 10^seq(-3,3.5,by=0.1)
+delta <- 1e3
+L <- 1.6e6
+
+plot(rho, expectedCount(rho, delta, L), 'l', lwd=2,
+     xlab=expression(paste(rho,"'",sep="")),
+     ylab='Recombination count',
+     ylim=c(0,1500))
+lines(rho, rho/2, lty=2, lwd=2);
+
+legend('topleft', inset=.05, c('No overlaps', 'With overlaps'),
+       lty=c(1,2), )
+
+
+dev.off()
