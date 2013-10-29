63e693458f8742792a76e2826eec0159cacec0b6
Tim Vaughan <tgvaughan@gmail.com>
2013-10-29 17:35:30 +1300

    Working on add/remove operator.

--- src/argbeast/operators/AddRemoveRecombination.java
+++ src/argbeast/operators/AddRemoveRecombination.java
@@ -1,10 +1,13 @@
 package argbeast.operators;
 
 
+import argbeast.Recombination;
+import argbeast.RecombinationGraph;
 import beast.core.Description;
 import beast.core.Input;
 import beast.core.Input.Validate;
 import beast.core.parameter.RealParameter;
+import beast.util.Randomizer;
 
 /*
  * Copyright (C) 2013 Tim Vaughan <tgvaughan@gmail.com>
@@ -35,14 +38,54 @@ public class AddRemoveRecombination extends RecombinationGraphOperator {
     public Input<RealParameter> deltaInput = new Input<RealParameter>("delta",
             "Tract length parameter.", Validate.REQUIRED);
     
+    private RecombinationGraph arg;
+    
     public AddRemoveRecombination() { };
     
     @Override
-    public void initAndValidate() { };
+    public void initAndValidate() {
+        arg = argInput.get();
+    };
 
     @Override
     public double proposal() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double logHGF = 0;
+
+        if (Randomizer.nextDouble()<0.5) {
+            
+            // Add
+            
+            logHGF += Math.log(1.0/(arg.getNRecombs()+1));
+            logHGF -= drawNewRecomb();
+            
+        } else {
+            
+            // Remove
+            
+            // Select recombination to remove:
+            Recombination recomb = arg.getRecombinations().get(Randomizer.nextInt(arg.getNRecombs())+1);
+            
+            // Calculate HGF
+            logHGF += getRecombProb(recomb);
+            logHGF -= Math.log(1.0/arg.getNRecombs());
+            
+            // Remove recombination
+            arg.deleteRecombination(recomb);
+        }
+        
+        return logHGF;
+    }
+    
+    private double drawNewRecomb() {
+        double logP = 0;
+        
+        return logP;
+    }
+    
+    private double getRecombProb(Recombination recomb) {
+        double logP = 0;
+        
+        return logP;
     }
     
 }
