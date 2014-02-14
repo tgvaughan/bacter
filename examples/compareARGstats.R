99debf9135fa1ec6c035f08aef64e328539c56f5
Tim Vaughan <tgvaughan@gmail.com>
2014-02-14 15:36:24 +1300

    Updated R compare script.

--- examples/compareARGstats.R
+++ examples/compareARGstats.R
@@ -8,7 +8,7 @@ removeBurnin <- function(df, burninFrac=0.1) {
     return(df[-(1:burnin),])
 }
 
-compareStats <- function(df1, df2, label1="df1", label2="df2") {
+compareStats <- function(df1, df2, label1="df1", label2="df2", noCF=FALSE) {
 
     par(mfrow=c(2,3))
 
@@ -37,9 +37,11 @@ compareStats <- function(df1, df2, label1="df1", label2="df2") {
            main='Edge length')
     lines(c(0,1e10), c(0,1e10))
 
-    qqplot(df1$arg.CFheight, df2$arg.CFheight,
-           xlab=label1, ylab=label2, col='blue',
-           main='Clonal frame height')
-    lines(c(0,1e10), c(0,1e10))
+    if (!noCF) {
+        qqplot(df1$arg.CFheight, df2$arg.CFheight,
+               xlab=label1, ylab=label2, col='blue',
+               main='Clonal frame height')
+        lines(c(0,1e10), c(0,1e10))
+    }
 }
 
