9abecb7dc5a7718cc385b3573f8fd3331a7746da
Tim Vaughan <tgvaughan@gmail.com>
2015-01-08 18:57:52 +1300

    Working on overlapping MarginalTree test.

--- test/bacter/MarginalTreeTest.java
+++ test/bacter/MarginalTreeTest.java
@@ -52,6 +52,7 @@ public class MarginalTreeTest extends TestBase {
         System.out.println(arg.getExtendedNewick(true));
         
         // Test all marginals against truth
+        // (I have eyeballed each of these trees and claim that they are correct.)
         String[] correctNewickStrings = {
             "(((0:0.04916909893812016,1:0.04916909893812016)10:0.5465237639426681,(((((3:0.07561592852503529,6:0.07561592852503529)11:0.1461919778724432,8:0.2218079063974785)13:0.010206467073885506,9:0.232014373471364)14:0.11654268918790511,(5:0.1074670293493194,7:0.1074670293493194)12:0.24109003330994971)15:0.028754070027424694,4:0.3773111326866938)16:0.21838173019409446)17:1.1073878800617445,2:1.7030807429425328)18:0.0",
             "((2:0.42839862922656696,(0:0.04916909893812016,1:0.04916909893812016)10:0.3792295302884468)18:0.1672942336542213,(((((3:0.07561592852503529,6:0.07561592852503529)11:0.1461919778724432,8:0.2218079063974785)13:0.010206467073885506,9:0.232014373471364)14:0.11654268918790511,(5:0.1074670293493194,7:0.1074670293493194)12:0.24109003330994971)15:0.028754070027424694,4:0.3773111326866938)16:0.21838173019409446)17:0.0",
@@ -71,12 +72,50 @@ public class MarginalTreeTest extends TestBase {
         }
 
         for (int r=0; r<arg.getConversions().size(); r++) {
-            Conversion recomb = arg.getConversions().get(r);
-            
             assertTrue(treesEquivalent(
                 new MarginalTree(arg, regionList.getRegions().get(r)).getRoot(),
                 new TreeParser(correctNewickStrings[r], false, true, false, 0).getRoot(),
                 1e-15)); 
+        }
+    }
+
+    @Test
+    public void testOverlapping() throws Exception {
+
+        // Conversion graph
+        String str = "[&2,1000,0.3260126313706676,10,5000,0.42839862922656696] "
+                + "[&10,2000,0.3381366423491633,2,8000,0.5683827224649434] "
+                + "[&10,3000,0.2807615297583804,14,7000,0.3415740002783274] "
+                + "[&2,7500,0.1,2,9999,0.4] "
+                + "(((0:0.04916909893812008,1:0.04916909893812008)10:0.5465237639426681,"
+                + "(4:0.3773111326866937,(((8:0.22180790639747835,"
+                + "(3:0.07561592852503513,6:0.07561592852503513)11:0.14619197787244323)"
+                + "13:0.010206467073885589,9:0.23201437347136394)14:0.116542689187905,"
+                + "(7:0.10746702934931932,5:0.10746702934931932)12:0.24109003330994963)"
+                + "15:0.02875407002742475)16:0.21838173019409446)17:1.1073878800617445,"
+                + "2:1.7030807429425328)18:0.0";
+        
+        ConversionGraph arg = new ConversionGraph();
+        arg.initByName("fromString", str, "sequenceLength", 10000);
+        System.out.println(arg.getExtendedNewick(true));
+        
+        // Test all marginals against truth
+        // (I have eyeballed each of these trees and claim that they are correct.)
+//        String[] correctNewickStrings = { };
+
+        RegionList regionList = new RegionList(arg);
+
+        for (Region region : regionList.getRegions()) {
+            System.out.println(region);
+            System.out.println(region.activeConversions);
+            System.out.println(new MarginalTree(arg, region) + ";");
+        }
+
+        for (int r=0; r<arg.getConversions().size(); r++) {
+//            assertTrue(treesEquivalent(
+//                new MarginalTree(arg, regionList.getRegions().get(r)).getRoot(),
+//                new TreeParser(correctNewickStrings[r], false, true, false, 0).getRoot(),
+//                1e-15)); 
         }
     }
 }
