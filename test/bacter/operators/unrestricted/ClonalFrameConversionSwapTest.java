bafd1a1145055906480f253d59ec17cf16e459b1
Tim Vaughan <tgvaughan@gmail.com>
2015-03-17 18:27:19 +1300

    Using CF/conv swap test to diagnose HR prob.

--- test/bacter/operators/unrestricted/ClonalFrameConversionSwapTest.java
+++ test/bacter/operators/unrestricted/ClonalFrameConversionSwapTest.java
@@ -16,9 +16,13 @@
  */
 package bacter.operators.unrestricted;
 
+import bacter.model.unrestricted.ACGCoalescent;
 import bacter.model.unrestricted.SimulatedACG;
+import beast.core.OperatorSchedule;
+import beast.core.State;
 import beast.core.parameter.RealParameter;
 import beast.evolution.tree.coalescent.ConstantPopulation;
+import beast.util.Randomizer;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -29,21 +33,36 @@ import org.junit.Test;
  */
 public class ClonalFrameConversionSwapTest {
 
-    public ClonalFrameConversionSwapTest() { }
+    public ClonalFrameConversionSwapTest() {
+    }
 
     @Test
     public void testHR() throws Exception {
+
+        Randomizer.setSeed(13);
 
         ConstantPopulation popFunc = new ConstantPopulation();
         popFunc.initByName("popSize", new RealParameter("1.0"));
 
         SimulatedACG acg = new SimulatedACG();
         acg.initByName(
-            "rho", 0.0001,
-            "delta", 500.0,
-            "populationModel", popFunc,
-            "nTaxa", 2,
-            "sequenceLength", 10000);
+                "rho", 0.0001,
+                "delta", 500.0,
+                "populationModel", popFunc,
+                "nTaxa", 2,
+                "sequenceLength", 10000);
+
+        ACGCoalescent target = new ACGCoalescent();
+        target.initByName(
+                "acg", acg,
+                "populationModel", popFunc,
+                "rho", new RealParameter("0.0001"),
+                "delta", new RealParameter("500.0"));
+
+        State state = new State();
+        state.initByName("stateNode", acg);
+        state.initialise();
+        state.setPosterior(target);
 
         /*
          ConversionGraph acg = new ConversionGraph();
@@ -54,10 +73,14 @@ public class ClonalFrameConversionSwapTest {
          */
         ClonalFrameConversionSwap operator = new ClonalFrameConversionSwap();
         operator.initByName(
-            "weight", 1.0,
-            "acg", acg,
-            "populationModel", popFunc,
-            "delta", new RealParameter("50.0"));
+                "weight", 1.0,
+                "acg", acg,
+                "populationModel", popFunc,
+                "delta", new RealParameter("50.0"));
+
+        OperatorSchedule operatorSchedule = new OperatorSchedule();
+        operatorSchedule.initByName();
+        operator.setOperatorSchedule(operatorSchedule);
 
         double logHR1, logHR2;
 
@@ -66,18 +89,26 @@ public class ClonalFrameConversionSwapTest {
             logHR1 = operator.createConversion();
         } while (Double.isInfinite(logHR1));
 
-        System.out.println(acg.getExtendedNewick(true));
+        operator.accept();
+        state.store(0);
+        state.acceptCalculationNodes();
 
+        System.out.println(acg.getExtendedNewick(true));
         do {
             logHR2 = operator.deleteConversion();
-        } while (Double.isInfinite(logHR2));
+            if (Math.abs(logHR1+logHR2)>1e-10) {
+                operator.reject();
+                state.restore();
+            }
+            else
+                break;
+        } while (true);
 
         System.out.println(acg.getExtendedNewick(true));
-
         System.out.println("logHR1 = " + logHR1);
         System.out.println("logHR2 = " + logHR2);
 
-        assertTrue(Math.abs(logHR1+logHR2)<1e-10);
+        assertTrue(Math.abs(logHR1 + logHR2) < 1e-10);
     }
-    
+
 }
