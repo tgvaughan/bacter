c52dd40e29e83df3374efef1cab0167cf11296dc
Tim Vaughan <tgvaughan@gmail.com>
2015-06-28 12:51:26 +1200

    Moved annotator classes to own package.

--- src/bacter/acgannotator/ACGLogFileReader.java
+++ src/bacter/acgannotator/ACGLogFileReader.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bacter.util;
+package bacter.acgannotator;
 
 import bacter.ConversionGraph;
 import bacter.Locus;
