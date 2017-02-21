bd44bbd91f6ffb7c5f629cec2efcc23f18b79883
Tim Vaughan <tgvaughan@gmail.com>
2017-02-22 08:23:57 +1300

    Added code for cumulative pairwise distance experiments.

---
+++ src/bacter/devutils/ComputeCPDMatrix.java
@@ -0,0 +1,100 @@
+package bacter.devutils;
+
+import bacter.*;
+import beast.core.*;
+import beast.core.parameter.RealParameter;
+import beast.evolution.alignment.Alignment;
+import beast.evolution.tree.Node;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.*;
+
+/**
+ * @author Tim Vaughan <tgvaughan@gmail.com>
+ */
+@Description("Approximate ACG likelihood using pairwise distances.")
+public class ComputeCPDMatrix extends beast.core.Runnable {
+
+    public Input<Alignment> alignmentInput = new Input<>(
+            "alignment",
+            "Alignment corresponding to particular locus.",
+            Input.Validate.REQUIRED);
+
+    public Input<String> outFileNameInput = new Input<>(
+            "outFileName",
+            "Name of output file",
+            Input.Validate.REQUIRED);
+
+    private int[][] cumulativeHD;
+    private Alignment alignment;
+
+    public ComputeCPDMatrix() { }
+
+    @Override
+    public void initAndValidate() {
+
+        alignment = alignmentInput.get();
+        computePairwiseDistances();
+    }
+
+    @Override
+    public void run() throws Exception {
+
+        try (PrintStream ps = new PrintStream(outFileNameInput.get())) {
+
+            ps.print("site");
+
+            for (int p=0; p<cumulativeHD.length; p++)
+                ps.print("\tp" + p);
+
+            ps.println();
+
+            for (int site=0; site<alignment.getSiteCount(); site++) {
+
+                ps.print(site);
+
+                for (int p=0; p<cumulativeHD.length; p++) {
+
+                        ps.print("\t" + cumulativeHD[p][site]);
+                }
+                ps.println();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error writing output file '"
+                    + outFileNameInput.get() + "'.");
+        }
+    }
+
+    private void computePairwiseDistances() {
+        // Pre-compute pairwise distance tables
+
+        int nSeqs = alignment.getTaxonCount();
+        int nPairs = nSeqs*(nSeqs-1)/2;
+        cumulativeHD = new int[nPairs][alignment.getSiteCount()+1];
+
+        for (int siteBoundary=0; siteBoundary<alignment.getSiteCount()+1; siteBoundary++) {
+            int pair = 0;
+            for (int tIdx1=0; tIdx1<nSeqs; tIdx1++) {
+                for (int tIdx2=tIdx1+1; tIdx2<nSeqs; tIdx2++) {
+                    if (siteBoundary == 0)
+                        cumulativeHD[pair][siteBoundary] = 0;
+                    else
+                        cumulativeHD[pair][siteBoundary] = cumulativeHD[pair][siteBoundary - 1];
+
+                    if (siteBoundary > 0) {
+                        int patternIdx = alignment.getPatternIndex(siteBoundary - 1);
+
+                        if (alignment.getPattern(tIdx1, patternIdx)
+                                != alignment.getPattern(tIdx2, patternIdx))
+                            cumulativeHD[pair][siteBoundary] += 1;
+                    }
+
+                    pair += 1;
+                }
+            }
+        }
+    }
+
+}
